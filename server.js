cf792e5396e976ea87d1d1b061a155ff16d99553
halaalsawaeer <h.sawaeer@yahoo.com>
2020-11-22 18:21:40 +0200

    up

--- server.js
+++ server.js
@@ -9,16 +9,46 @@ server.use(cors());
 
 const PORT=process.env.PORT|| 3000;
 let weather =require('./data/weather.json');
+
+
 server.get('/',(req,res)=>{
     res.send(weather);
 })
-function Weather(){
-
-
+Weather.all=[];
+function Weather(descreption,time){
+    this.forecast =descreption;
+    this.time =time;
+    Weather.all.push(this);
 }
+// let WArray=weather.data;
+// let descreption=weather.data[0].weather.description;
+//     let time=weather.data[0].datetime;
+// let whetherObj=new Weather(descreption,time);
+// console.log(typeof Object.values(weather.data));
+// console.log(Object.keys(weather.data[0]));
+//  WArray.forEach(element => {
+//     let descreption=weather.data[element].weather.description;
+//     let time=weather.data[element].datetime;
+// let whetherObj=new Weather(descreption,time);
+// console.log(whetherObj);
+// });
 
 
 
+server.get('/weather',(req,res)=>{
+    for(let i=0;i<weather.data.length;i++){
+        let descreption=weather.data[i].weather.description;
+        let time=weather.data[i].datetime;
+        console.log(time);
+      
+        let whetherObj=new Weather(descreption,time);
+        res.send(whetherObj);
+    }
+    console.log(Weather.all);
+
+
+
+})
 
 server.listen(PORT,()=>{
     console.log("Everything is good");
